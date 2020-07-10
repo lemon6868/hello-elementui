5ae5cd450b1def712a1a2fbe3a6c395e19754ad5
niurui <crazy.mon@qq.com>
2020-07-10 17:13:36 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <el-progress :text-inside="true" :stroke-width="26" :percentage="70"></el-progress>
+    <el-progress :text-inside="true" :stroke-width="24" :percentage="100" status="success"></el-progress>
+    <el-progress :text-inside="true" :stroke-width="22" :percentage="80" status="warning"></el-progress>
+    <el-progress :text-inside="true" :stroke-width="20" :percentage="50" status="exception"></el-progress>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
